ae851594f0c62c3f20335a64ad6577a2d3e11f83
Andrew Meng <menga@rose-hulman.edu>
2021-10-18 11:40:21 -0400

    added verifier with tests

---
+++ modules/generateInstruction/verifierAdapterTests.py
@@ -0,0 +1,22 @@
+import verifierAdapter
+
+def test_setISA():
+    verifierAdapter.setISA("mips32")
+    assert verifierAdapter.archType == verifierAdapter.CS_MODE_MIPS32 and verifierAdapter.mode == verifierAdapter.CS_MODE_MIPS32
+
+def test_initialize():
+    verifierAdapter.setISA("mips32")
+    verifierAdapter.initialize()
+    assert verifierAdapter.disassembler != -1
+
+def test_disassembleNonsense():
+    verifierAdapter.setISA("mips32")
+    verifierAdapter.initialize()
+    ret = verifierAdapter.isValidInstruction(b"\x00\x00\x00\x00")
+    assert not ret
+
+def test_disassembleValidAdd():
+    verifierAdapter.setISA("mips32")
+    verifierAdapter.initialize()
+    ret = verifierAdapter.isValidInstruction(b"\x01\x08\x40\x30")
+    assert ret
