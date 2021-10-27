a3b460376309c8eb9dcfb2f93565b5b22693c647
Andrew Meng <menga@rose-hulman.edu>
2021-10-27 13:54:35 -0400

    fixed issue with using capstone, finalized verifierAdapter

--- modules/generateInstruction/verifierAdapter.py
+++ modules/generateInstruction/verifierAdapter.py
@@ -1,50 +1,50 @@
 from capstone import *
 from capstone.mips import *
 
-#CODE = b"000000010000100001000000 00100000" #normal
-#CODE = b"01000000010000000000100000000001" #reversed
-# CODEB = b"\xa1\x08\x40\x30"
-# CODE = b"\x30\x40\x08\x01"
-#CODE = b"\x20\x08\x08\x08"
-       #xb8\x13\x00\x00"
-
-#b"\x01\x4b\x48\x20"
+# do not access these variables directly from outside this file; use getters instead
 disassembler = -1
 archType = -1
 mode = -1
-littleEndian = False
+littleEndian = False # default to big-endian
 
-def setIsLittleEndian(isLittleEndian):
+# setter for littleEndian
+def setLittleEndian(isLittleEndian):
 	global littleEndian
 	littleEndian = not not isLittleEndian
 
-def getIsLittleEndian():
+# getter for littleEndian
+def getLittleEndian():
 	return littleEndian
 
+# define the ISA to use; e.g. mips32
 def setISA(architecture):
 	isa = architecture.lower()
 	global archType
 	global mode
 	if isa == "mips32":
-		archType = CS_MODE_MIPS32
+		archType = CS_ARCH_MIPS
 		mode = CS_MODE_MIPS32
 		return
 	else:
 		print("choose mips32, mips64, x86, etc")
 		return
 	
+# getter for archType
 def getArchType():
 	return archType
 
+# getter for mode
 def getMode():
 	return mode
 
+# getter for disassembler
 def getDisassembler():
 	return disassembler
 
+# instantiate the Capstone object (disassembler)
 def initialize():
 	global disassembler
-	if archType == -1 or mode == -1:
+	if archType == -1 or mode == -1: # then the architecture has not been chosen
 		print("call setISA() with an architecture")
 		return
 	if littleEndian:
@@ -53,12 +53,11 @@ def initialize():
 		return
 	else:
 		disassembler = Cs(archType, mode+CS_MODE_BIG_ENDIAN)
-		print("Made with big endian")
 		disassembler.detail = True
 		return
 
+# given binary code, decide whether it is a valid instruction
 def isValidInstruction(instruction, verbose=False):
-	print(4)
 	if disassembler == -1:
 		print("you still need to initialize")
 		return False
--- tests/generateInstructionTests/verifierAdapterTests.py
+++ tests/generateInstructionTests/verifierAdapterTests.py
@@ -1,50 +1,93 @@
 from modules.generateInstruction.verifierAdapter import *
 import pytest
 
-# def test_setISA():
-#     setISA("mips32")
-#     assert archType == CS_MODE_MIPS32 and mode == CS_MODE_MIPS32
+"""
+This file tests the verifierAdapter file. The verifierAdapter file is intended to describe
+whether a given instruction is a valid instruction or not given an architecture. For example,
+given that it is to use the mips32 archticture and asked whether b'\x00\x00\x00\x00' is a valid
+instruction, it should return true because that string represents the no-op instruction.
 
-# def test_initialize():
-#     setISA("mips32")
-#     initialize()
-#     assert disassembler != -1
+The following valid instructions are tested in the order given:
+no-op
+add
+sub
+sll
+srl
+or
+and
 
+Additionally, test an instruction composed of all 1s (0xffffffff)
+to verify that invalid instructions are properly rejected
+"""
+
+# verify that the archType and mode variables are being set correctly
+def test_setISA():
+    setISA("mips32")
+    assert getArchType() == CS_ARCH_MIPS and getMode() == CS_MODE_MIPS32
+
+# verify that the disassembler (Capstone object) is being set correctly
+def test_initialize():
+     setISA("mips32")
+     initialize()
+     assert getDisassembler() != -1
+
+# initialize the Capstone object
 def init_mips32():
     setISA("mips32")
     initialize()
-    setIsLittleEndian(False)
     return
 
-
-def test_disassembleNonsense():
+# test the no-op instruction
+def testNoOp():
     init_mips32()
     ret = isValidInstruction(b"\x00\x00\x00\x00")
-    assert not ret
-
-def test_disassembleValidAdd():
-    init_mips32()
-    ret = isValidInstruction(b"\x08\x01\x20\x40")
-    # ret = isValidInstruction(b"\x01\x08\x40\x20")
     assert ret
 
-def test_disassembleValidSub():
+# test an add instruction (add t1 t2 t3)
+def testValidAdd():
     init_mips32()
-    print("test_disassembleV")
-    # sub t0 t0 t0
-    instruction = b"\x01\x4b\x48\x22"
-    # ret = isValidInstruction(b"\x08\x01\x20\x44", True)
-    # ret = isValidInstruction(b"\x01\x08\x40\x22", True)
+    instruction = b"\x01\x4b\x48\x20"
     ret = isValidInstruction(instruction)
-    print(ret)
-    assert False
-
-def test_disassembleValidSll():
-    init_mips32()
-    # shift left logical - first register all zeroes, ignored.
-    # sll t0 t1 1 
-    # 000000 00000 01000 01001 00001 000000
-    # 00000000 00001000 01001000 01000000
-    # ret = isValidInstruction(b"\x00\x01\x21\x20")
-    ret = isValidInstruction(b"\x00\x08\x48\x40", True)
     assert ret
+
+# test a sub instruction (sub t1 t2 t3)
+def testValidSub():
+    init_mips32()
+    instruction = b"\x01\x4b\x48\x22"
+    ret = isValidInstruction(instruction)
+    assert ret
+
+# test a shift left logical instruction (sll t0 t1 1)
+def testValidSll():
+    init_mips32()
+    instruction = b"\x00\x09\x40\x40"
+    ret = isValidInstruction(instruction)
+    assert ret
+
+# test a shift right logical instruction (srl t0 t1 1)
+def testValidSrl():
+    init_mips32()
+    instruction = b"\x00\x09\x40\x42"
+    ret = isValidInstruction(instruction)
+    assert ret
+
+# test a valid or instruction (or t1 t2 t3)
+def testValidOr():
+    init_mips32()
+    instruction = b"\x01\x4b\x48\x25"
+    ret = isValidInstruction(instruction)
+    assert ret
+
+# test a valid and instruction (and t1 t2 t3)
+def testValidAnd():
+    init_mips32()
+    instruction = b"\x01\x4b\x48\x24" 
+    ret = isValidInstruction(instruction)
+    assert ret
+
+# test a string that does not represent an instruction (32 bits of only ones)
+def testInvalidInstruction():
+    init_mips32()
+    instruction = b"\xff\xff\xff\xff"
+    ret = isValidInstruction(instruction)
+    assert not ret
