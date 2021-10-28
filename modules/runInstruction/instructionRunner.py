6ad64ebef83a55f2d381f0d5d4ba7a4543fae6fa
Jake Milanowski <milanoj@rose-hulman.edu>
2021-10-28 12:03:08 -0400

    Instruction Runner written, test file created

---
+++ modules/runInstruction/instructionRunner.py
@@ -0,0 +1,30 @@
+from modules.generateInstruction import instructionGenerator as instructionGen
+from modules.runInstruction.stateManager import initializePanda
+
+def generateInstructionData(arch="mips", instructionTotal=1, instructionIterations=10, verbose=False):
+    """Generates a structure of data pertaining to randomly generated instructions in a set ISA.
+
+    Arguments:
+        arch -- specifies the architecture to generate instruction data for (default = mips32)
+        Valid Arguments:
+            mips32 -- Use the MIPS architecture
+
+        instructionTotal -- specifies the total number of random instructions to generate data for (default = 1)
+
+        instructionIterations -- specifies the number times an instruction is run and data is generated (default = 10)
+
+    Output:
+        Returns a dictionary of <instruction bytecode> -> <result array> pairings where the result array is an <instructionIterations x 2> array 
+        with the first column specifying the register state before instruction execution and the second column specifying the register state afterwards.
+
+        Register state is stored as a dictionary of <register name> -> <register value> pairings.
+    """
+    pandaInstance = initializePanda(arch)
+    instructionGen.initialize(arch)
+
+    instructionList = []
+
+    for _ in range(instructionTotal):
+        instructionList.append(instructionGen.generateInstruction())
+
+    return runInstructions(pandaInstance, instructionList, instructionIterations, verbose)
