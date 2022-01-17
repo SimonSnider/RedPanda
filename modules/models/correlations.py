10dcabf3a765247d97013738f66f76e63139fcaa
Danielle Villa <villadn@rose-hulman.edu>
2022-01-17 13:52:10 -0500

    data class

---
+++ modules/models/correlations.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass, field
+
+@dataclass
+class Correlations:
+	regToReg: "list[list[float]]" = field(default_factory=list)
+	regToReadAddress: "list[list[float]]" = field(default_factory=list)
+	regToWriteAddress: "list[list[float]]" = field(default_factory=list)
+	regToWriteData: "list[list[float]]" = field(default_factory=list)
+	readDataToReg: "list[list[float]]" = field(default_factory=list)
+
+@dataclass
+class IntermediateData:
+	initialInput: "list[int]" = field(default_factory=list)
+	initialOutput: "list[int]" = field(default_factory=list)
+	inputs: "list[int]" = field(default_factory=list)
+	outputs:"list[int]" = field(default_factory=list)
+	ps: "list[int]" = field(default_factory=list)
