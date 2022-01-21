7d8c9d6039fb980268d843f6d8e0984ed9ad3421
Jake Milanowski <milanoj@rose-hulman.edu>
2022-01-21 16:48:13 -0500

    intermediate runner minor changes

--- modules/intermediateRunner.py
+++ modules/intermediateRunner.py
@@ -99,15 +99,30 @@ def runInputAndModel():
     print("Specify the analysis model (default = 0)")
     print("Supported Models")
     print("    0 - reg-coorelational")
+    print("    1 - mem-reg-coorelational")
     try:
         model = int(input() or 0)
     except ValueError:
         print("Value supplied not numerical. Please supply a numeric value corresponding to a supported model.")
         return
 
-    if model != 0:
+    if model < 0 or model > 1:
         print("Model not within supported range. Please enter a supported model value.")
-        return 
+        return
+
+    # Output Form
+    print("Specify the output form (default = 0)")
+    print("Supported Forms")
+    print("    0 - coorelation matrix")
+    try:
+        form = int(input() or 0)
+    except ValueError:
+        print("Value supplied not numerical. Please supply a numeric value corresponding to a supported output form.")
+        return
+
+    if form < 0 or form > 1:
+        print("Output not within supported range. Please enter a supported output form value.")
+        return
 
     # Verbosity
     print("Verbose? (default = 0)")
@@ -121,14 +136,14 @@ def runInputAndModel():
         print("Value supplied is not either 0 or 1. Please supply a valid value.")
         return
 
-    runModel(arch, mode, instructionIterations, outputFileName, instructionsFile, numInstructions, verbose)
+    runModel(arch, mode, instructionIterations, outputFileName, instructionsFile, numInstructions, form, verbose)
 
 
 
-def runModel(arch, mode, instructionIterations, outputFileName, instructionsFile = "", numInstructions = 1, verbose = 0):
+def runModel(arch, mode, instructionIterations, outputFileName, instructionsFile = "", numInstructions = 1, form = 0, verbose = 0):
     #
     # Generate instructions or load them from a file
-    #
+    # 
     if mode == 0:
         # Instructions are generated randomly using the generateInstruction module
         instructionList = []
@@ -233,5 +248,7 @@ if len(sys.argv) > 1:
             )
 
         runModel(arguments[0], int(arguments[1]), int(arguments[2]), arguments[3], arguments[4], int(arguments[5]), int(arguments[6]))
+    else:
+        runInputAndModel()
 else:
     runInputAndModel()
