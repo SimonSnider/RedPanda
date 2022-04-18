0d88231738631e9132a37564a1f06092e86e6c86
Danielle Villa <villadn@rose-hulman.edu>
2022-04-17 23:40:08 -0400

    minor changes

--- tests/runAllTests.py
+++ tests/runAllTests.py
@@ -6,31 +6,31 @@ def ctt():
     try:
         test.test1()
         numSuccess += 1
-    except AssertionError:
+    except :
         print("compareToTaintTests test1 failed!")
 
     try:
         test.test2()
         numSuccess += 1
-    except AssertionError:
+    except :
         print("compareToTaintTests test2 failed!")
 
     try:
         test.test3()
         numSuccess += 1
-    except AssertionError:
+    except :
         print("compareToTaintTests test3 failed!")
 
     try:
         test.testModelCollection()
         numSuccess += 1
-    except AssertionError:
+    except :
         print("compareToTaintTests testModelCollection failed!")
 
     print(numSuccess, " tests passed in compareToTaintTests.")
         
 if __name__ == "__main__":
-    t1 = t.Thread(target=ctt,args=())
+    t1 = thread.Thread(target=ctt,args=())
 
     t1.start()
 
