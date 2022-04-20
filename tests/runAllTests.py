0cf17af3961f219f48d68798af53d673619c8241
Simon Snider <snidersa@rose-hulman.edu>
2022-04-20 10:57:18 -0400

    state manager mips tests added to runAll Tests

--- tests/runAllTests.py
+++ tests/runAllTests.py
@@ -28,13 +28,65 @@ def ctt():
         print("compareToTaintTests testModelCollection failed!")
 
     print(numSuccess, " tests passed in compareToTaintTests.")
+    
+def stateManagerMipsTests():
+    numTests = 0
+    numSuccesses = 0
+    from runInstructionTests import stateManagerMipsTestsForThreading as smt
+    smt.runPanda()
+    try:
+        numTests += 1
+        smt.testRandomizeRegisterState()
+        numSuccesses += 1
+    except AssertionError:
+        print("state manager mips test testRandomizeRegisterState failed!")
         
-if __name__ == "__main__":
-    t1 = t.Thread(target=ctt,args=())
+    try:
+        numTests += 1
+        smt.testOffLimitsRegs()
+        numSuccesses += 1
+    except AssertionError:
+        print("state manager mips test testOffLimitsRegs failed!")
+        
+    try:
+        numTests += 1
+        smt.testGetBitTrue()
+        numSuccesses += 1
+    except AssertionError:
+        print("state manager mips test testGetBitTrue failed!")
+        
+    try:
+        numTests += 1
+        smt.testGetBitFalse()
+        numSuccesses += 1
+    except AssertionError:
+        print("state manager mips test testGetBitFalse failed!")
+        
+    try:
+        numTests += 1
+        smt.testRandomizeRegisterWithBitmask()
+        numSuccesses += 1
+    except AssertionError:
+        print("state manager mips test testRandomizeRegisterWithBitmask failed!")
+    try:
+        numTests += 1
+        smt.testSetRegisters()
+        numSuccesses += 1
+    except AssertionError:
+        print("state manager mips test testSetRegisters failed!")
 
+    print("{} / {} tests passed in stateManagerMipsTests".format(numSuccesses, numTests))
+    
+
+        
+
+if __name__ == "__main__":
+    t1 = thread.Thread(target=ctt,args=())
+    t2 = thread.Thread(target=stateManagerMipsTests, args=())
     t1.start()
+    t2.start()
 
     t1.join()
-
+    t2.join()
     print("All tests compete.")
 
