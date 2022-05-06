5a9d2a6e3320972c63cd3e638cc0ececc827b7d5
Andrew Meng <menga@rose-hulman.edu>
2022-05-06 15:54:18 -0400

    updating comparisonOutput

--- red_panda/create_output/comparisonOutput.py
+++ red_panda/create_output/comparisonOutput.py
@@ -1,7 +1,7 @@
 from red_panda.models.correlations import *
 from red_panda.compare_to_taint.taintComparer import *
 
-def printAllCorrelations(corrDict, writeFile):
+def printAllCorrelations(corrDict, writeFile, registerNames):
     for reg in corrDict.keys():
         tainted = corrDict[reg]
         if(len(tainted) == 0):
@@ -10,22 +10,24 @@ def printAllCorrelations(corrDict, writeFile):
             for reg2 in tainted:
                 writeFile.write(f"Register {reg} affects register {reg2}.\n")
 
-def generateOutput(instructionNames, data, filename):
+def generateOutput(instructionNames, data, filename, registerNames):
     filename = filename + "Comparison.txt"
-    with open(filename, 'w') as f:
+    with open(filename, 'a') as f:
         ourModel = extractNewModel(data[0])
         pandaModel = convertMatrixToDict(data[1][0], 0.5)
 
-        f.write("Taint based on random testing:\n")
-        printAllCorrelations(ourModel, f)
-        f.write("\nTaint based on PANDA's taint system:\n")
-        printAllCorrelations(pandaModel, f)
+        f.write("\nInstruction: " + instructionNames + "\n")
+
+        f.write("Taint based on random testing:\n\n")
+        printAllCorrelations(ourModel, f, registerNames)
+        f.write("\nTaint based on PANDA's taint system:\n\n")
+        printAllCorrelations(pandaModel, f, registerNames)
 
         [pandaTainted, randomTainted] = compare(data[1], data[0])
         
         f.write("\nDifferences in taint models:\n")
 
-        if(len(pandaTainted) == 0 and len(randomTainted) == 0):
+        if(len(pandaTainted.keys()) == 0 and len(randomTainted.keys()) == 0):
             f.write("None\n")
         else:
             for corr in pandaTainted.keys():
@@ -34,6 +36,8 @@ def generateOutput(instructionNames, data, filename):
             for corr in randomTainted.keys():
                 for reg in randomTainted[corr]:
                     f.write(f"Random testing found that register {corr} affects register {reg}, while PANDA did not.\n")
+
+        f.write("---------------------------------------------\n")
 
         
         
